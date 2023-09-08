1e177428e5c12f92ff66b4dbacf4d3a7817c0b78
munimrahman <munimrh@gmail.com>
2023-09-08 22:38:05 +0600

    final

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1",
+    baseUrl: "https://task-management-server-munimrahman.vercel.app/api/v1",
   }),
   tagTypes: [],
   endpoints: (builder) => ({}),
